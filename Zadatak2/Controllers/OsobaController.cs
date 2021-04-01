0922e4b5413cc38dc4398a11b34b5ee8e5295bb0
Fran Anusic <fran.anusic@hotmail.com>
2021-04-01 15:18:42 +0200

    Add project files.

---
+++ Zadatak2/Controllers/OsobaController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zadatak2.Models;
+
+namespace Zadatak2.Controllers
+{
+    public class OsobaController : Controller
+    {
+        public ActionResult UnosOsobe()
+        {
+            return View(new Osoba() { DatumRodjenja = DateTime.Today });
+        }
+        [HttpPost]
+        public ActionResult ProvjeriOsobu(Osoba osoba)
+        {
+            if (string.IsNullOrEmpty(osoba.Ime))
+            {
+                ModelState.AddModelError("Ime", "Ime je obavezno!");
+            }
+            if (string.IsNullOrEmpty(osoba.Prezime))
+            {
+                ModelState.AddModelError("Prezime", "Prezime je obavezno!");
+            }
+            if (osoba.DatumRodjenja == DateTime.Today)
+            {
+                ModelState.AddModelError("DatumRodjenja", "Datum rođenja je obavezan!");
+            }
+            if (string.IsNullOrEmpty(osoba.Email))
+            {
+                ModelState.AddModelError("Email", "E-mail je obavezan!");
+            }
+            if (ModelState.IsValid)
+            {
+                return View("PregledOsobe", osoba);
+            }
+            else
+            {
+                return View("UnosOsobe", osoba);
+            }
+        }
+    }
+}
---
+++ Zadatak2/Models/Osoba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zadatak2.Models
+{
+    public class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public string Email { get; set; }
+    }
+}
